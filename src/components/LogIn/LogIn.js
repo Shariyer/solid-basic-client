bc0ef53ebc9642e35b60b0dcca9b72fdaac9885c
shariyer <s.m.shariyer04@gmail.com>
2022-10-25 18:01:35 +0600

    login and registration form complete

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,11 +1,50 @@
 /** @format */
 
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { NavLink } from "react-router-dom";
 
 const LogIn = () => {
+  const handleForm = (event) => {
+    const form = event.target;
+    const fullname = form.fullName.value;
+    const photoURL = form.PhotoURL.value;
+    const email = form.email.value;
+    const password = form.password.value;
+  };
+
   return (
-    <div>
-      <h5>this is login page</h5>
+    <div className="mt-5">
+      <Form className="w-50 mx-auto bg-dark bg-opacity-25 p-5 shadow rounded">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter Your email address"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Enter your Password"
+          />
+        </Form.Group>
+
+        <span className=" d-flex justify-content-center">
+          <Button className="px-5" variant="outline-dark" type="submit">
+            Log In
+          </Button>
+        </span>
+        <li>
+          <small>
+            New to this site?? <NavLink to="/register">Register</NavLink>
+          </small>
+        </li>
+      </Form>
     </div>
   );
 };
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,11 +1,70 @@
 /** @format */
 
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { NavLink } from "react-router-dom";
 
 const Register = () => {
+  const handleForm = (event) => {
+    const form = event.target;
+    const fullname = form.fullName.value;
+    const photoURL = form.PhotoURL.value;
+    const email = form.email.value;
+    const password = form.password.value;
+  };
+
   return (
     <div>
-      <h5>this is Register page</h5>
+      <Form className="w-50 mx-auto bg-dark bg-opacity-25 p-5 shadow rounded border">
+        <h2 className="text-center">Registration</h2>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="fullName"
+            placeholder="Enter Your full Name"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter Your email address"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Photo URL</Form.Label>
+          <Form.Control
+            type="text"
+            name="photoURL"
+            placeholder="Enter Your PhotoURL email"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Enter your Password"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Terms & Condition" />
+        </Form.Group>
+        <span className=" d-flex justify-content-center">
+          <Button className="px-5" variant="outline-dark" type="submit">
+            Register
+          </Button>
+        </span>
+        <li>
+          <small>
+            Already have an Account?? <NavLink to="/login">LogIn</NavLink>
+          </small>
+        </li>
+      </Form>
     </div>
   );
 };
