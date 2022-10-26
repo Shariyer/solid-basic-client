2f860971e69f0acdef6b7e8684d22bba97b32bfe
shariyer <s.m.shariyer04@gmail.com>
2022-10-27 04:31:51 +0600

    Home carusel added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,38 +1,70 @@
 /** @format */
 
 import React from "react";
-import { Container, Image } from "react-bootstrap";
+import { Carousel, Image } from "react-bootstrap";
 
 const Home = () => {
   return (
-    <div className="py-5">
-      <Container>
-        <div className="d-flex">
-          <Image src="logo.png"></Image>
-          <h4 className="m-5">
-            Welcome to{" "}
+    <div className="container p-1 px-3 py-5 rounded shadow text-white bg-dark bg-opacity-75">
+      <div className="text-center d-flex justify-content-center align-items-center">
+        <h1 className="fw-bolder  text-white px-4 py-4 rounded shadow ">
+          Solid Basic
+        </h1>
+        <Image src="logo.png"></Image>
+      </div>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 img-fluid"
+            src="cover.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h1 className="">Welcome To</h1>
             <span className="fs-1 fw-bolder border rounded px-4 bg-warning bg-opacity-75 text-white shadow">
               Solid Basic
             </span>
-          </h4>
-        </div>
-        <h1 className="m-5">
-          Website where you can find the{" "}
-          <span className="fs-1 text-danger fw-bolder">
-            Best quality Free courses
-          </span>
-        </h1>
-        <h1 className="m-5">
-          To get started with{" "}
-          <span className="fs-1 fw-bolder text-danger">
-            Basic programming knowledge
-          </span>
-        </h1>
-        <h1 className="m-5">
-          And it's Perfect for{" "}
-          <span className="text-danger fw-bolder">Beginners</span>
-        </h1>
-      </Container>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src="cover.jpg" alt="Second slide" />
+
+          <Carousel.Caption>
+            <h3>
+              {" "}
+              <span className="fs-1 text-danger fw-bolder">
+                Best quality Free courses
+              </span>
+            </h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src="cover.jpg" alt="Third slide" />
+
+          <Carousel.Caption>
+            <h1 className="">
+              Basic
+              <span className="fs-1 fw-bolder text-danger">
+                Programming knowledge
+              </span>
+            </h1>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src="cover.jpg" alt="Third slide" />
+
+          <Carousel.Caption>
+            <h1 className="">
+              And it's Perfect for{" "}
+              <span className="text-danger fw-bolder">Beginners</span>
+            </h1>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
+      {/* <div className="d-lg-none d-block">
+        <Categories></Categories>
+      </div> */}
     </div>
   );
 };
